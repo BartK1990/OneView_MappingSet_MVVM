2a55ef39e2975b7104aa30ab725b7075ab88df0c
Bartosz Kuriata <kuriata.bartosz@hotmail.com>
2021-04-05 15:22:54 +0200

    Icons added

--- OneView_MappingSet_MVVM.Model/StandardMappingSet.cs
+++ OneView_MappingSet_MVVM.Model/StandardMappingSet.cs
@@ -1,6 +1,11 @@
-﻿namespace OneView_MappingSet_MVVM.Model
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace OneView_MappingSet_MVVM.Model
 {
     public class StandardMappingSet
     {
+        public IDictionary<string, long> TagIndexDictionary;
+        public ICollection<Iec6140025Tag> TaglistCollection;
     }
 }
