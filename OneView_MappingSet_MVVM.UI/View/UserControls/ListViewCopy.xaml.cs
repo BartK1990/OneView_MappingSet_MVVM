2736d53d42d47e0892d30a6ba737b87f16406f66
Bartosz Kuriata <kuriata.bartosz@hotmail.com>
2021-04-11 10:11:18 +0200

    - New user control - ListView with copy option

--- OneView_MappingSet_MVVM.UI/View/UserControls/ListViewCopy.xaml.cs
+++ OneView_MappingSet_MVVM.UI/View/UserControls/ListViewCopy.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace OneView_MappingSet_MVVM.UI.View.UserControl
+namespace OneView_MappingSet_MVVM.UI.View.UserControls
 {
     /// <summary>
     /// Interaction logic for ListViewCopy.xaml
