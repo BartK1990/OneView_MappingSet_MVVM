3dd77d251f48a1a1f0557a168e1b25f73f272673
Bartosz Kuriata <kuriata.bartosz@hotmail.com>
2021-05-15 12:55:42 +0200

    - Added method returning excel file type

--- OneView_MappingSet_MVVM.Model/MappingSetGenerator.cs
+++ OneView_MappingSet_MVVM.Model/MappingSetGenerator.cs
@@ -1,22 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace OneView_MappingSet_MVVM.Model
 {
     public enum ExcelFileType
     {
-        Invalid = -1,
-        StandardTagList = 1
+        Invalid = -1
+        , StandardTagList = 1
+        , SourceDictionary = 2
+        , SourceList = 3
     }
 
     public class MappingSetGenerator
     {
         public const string StandardTagListSheetName = "SCI Standard Mappingset";
+        public const string SourceItemDictionarySheetName = "SCI Source Dictionary";
+        public const string SourceItemListSheetName = "SCI Source Items";
 
-        public ExcelFileType CheckExcelFileType(string path) 
+        public ExcelFileType CheckIfConatainsValidSheet(ICollection<string> sheetCollection) 
         {
-            throw new NotImplementedException();
+            foreach (string sheetName in sheetCollection)
+            {
+                switch (sheetName)
+                {
+                    case(StandardTagListSheetName):
+                        return ExcelFileType.StandardTagList;
+                    case (SourceItemDictionarySheetName):
+                        return ExcelFileType.SourceDictionary;
+                    case (SourceItemListSheetName):
+                        return ExcelFileType.SourceList;
+                    default:
+                        break;
+                }
+            }
+            return ExcelFileType.Invalid;
+        }
+
+        public async Task<ExcelFileType> CheckIfConatainsValidSheetAsync(ICollection<string> sheetCollection)
+        {
+            return await Task.Run(() => CheckIfConatainsValidSheet(sheetCollection));
         }
     }
 }
