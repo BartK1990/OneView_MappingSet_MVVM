0fdb82e0c096950902337598be2d072dc184f9a7
Bartosz Kuriata <kuriata.bartosz@hotmail.com>
2021-06-30 16:22:38 +0200

    - Added test project - Small cleaning

--- OneView_MappingSet_MVVM.DataAccess/SourceItemListExcelAccess.cs
+++ OneView_MappingSet_MVVM.DataAccess/SourceItemListExcelAccess.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace OneView_MappingSet_MVVM.DataAccess
+﻿namespace OneView_MappingSet_MVVM.DataAccess
 {
     class SourceItemListExcelAccess
     {
---
+++ OneView_MappingSet_MVVM.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace OneView_MappingSet_MVVM.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
