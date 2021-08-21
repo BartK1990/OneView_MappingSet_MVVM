d45a0057f72f4a76c816961cd4f33557d6c54556
Bartosz Kuriata <kuriata.bartosz@gmail.com>
2021-08-21 05:12:59 +0200

    Excel sheet style adjusted

--- OneView_MappingSet_MVVM.DataAccess/MappingSetCreateExcelAccess.cs
+++ OneView_MappingSet_MVVM.DataAccess/MappingSetCreateExcelAccess.cs
@@ -13,23 +13,31 @@ namespace OneView_MappingSet_MVVM.DataAccess
 
             PrepareSheet(ws);
 
-            ws.Cells["A1:Y1"].AutoFitColumns();
+            ws.Cells["A1:Y2"].AutoFitColumns();
             package.Save();
             return null;
         }
 
         private static void PrepareSheet(ExcelWorksheet ws)
         {
+            var greenHtmlColor = @"#90EE90";
+            var redHtmlColor = @"#FFB6C1";
+
             ws.Cells["A1"].Value = "MappingTag";
+            ws.Cells["A1:H1"].Merge = true;
 
             ws.Cells["A2"].Value = "Name";
-            ws.Cells["B2"].Value = "Presentation Name";
-            ws.Cells["C2"].Value = "Presentation Name";
-            ws.Cells["D2"].Value = "Presentation Name";
-            ws.Cells["E2"].Value = "Presentation Name";
+            ws.Cells["B2"].Value = "Presentation Name\r\n(English)";
+            ws.Cells["C2"].Value = "Presentation Name\r\n(German)";
+            ws.Cells["D2"].Value = "Presentation Name\r\n(Spanish)";
+            ws.Cells["E2"].Value = "Presentation Name\r\n(Polish)";
             ws.Cells["F2"].Value = "SIType";
             ws.Cells["G2"].Value = "DataType";
             ws.Cells["H2"].Value = "Description";
+
+            ws.Cells["A1:H2"].Style.Fill.PatternType = ExcelFillStyle.Solid;
+            ws.Cells["A1:H2"].Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml(greenHtmlColor));
+            
             ws.Cells["I2"].Value = "SourceItemIdentifier";
             ws.Cells["J2"].Value = "SourceItemType";
             ws.Cells["K2"].Value = "SIType\r\n(depricated)";
@@ -40,17 +48,46 @@ namespace OneView_MappingSet_MVVM.DataAccess
             ws.Cells["P2"].Value = "IsStatusTag\r\n(depricated)";
             ws.Cells["Q2"].Value = "QualityCondition\r\n(depricated)";
             ws.Cells["R2"].Value = "ExpressionModel";
+
+            ws.Cells["I2:R2"].Style.Fill.PatternType = ExcelFillStyle.Solid;
+            ws.Cells["I2:J2"].Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml(greenHtmlColor));
+            ws.Cells["K2"].Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml(redHtmlColor));
+            ws.Cells["L2:O2"].Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml(greenHtmlColor));
+            ws.Cells["P2:Q2"].Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml(redHtmlColor));
+            ws.Cells["R2"].Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml(greenHtmlColor));
+
+            ws.Cells["S1"].Value = "ExpressionModel2123";
+            ws.Cells["S1:U1"].Merge = true;
+
             ws.Cells["S2"].Value = "Type";
             ws.Cells["T2"].Value = "MappingSetTagValueId";
             ws.Cells["U2"].Value = "Expression";
+
+            ws.Cells["S1:U2"].Style.Fill.PatternType = ExcelFillStyle.Solid;
+            ws.Cells["S1:U2"].Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml(greenHtmlColor));
+
+            ws.Cells["V1"].Value = "WriteExpression";
+            ws.Cells["V1:X1"].Merge = true;
+
             ws.Cells["V2"].Value = "Type";
             ws.Cells["W2"].Value = "MappingSetTagValueId";
             ws.Cells["X2"].Value = "Expression";
+
+            ws.Cells["V1:X2"].Style.Fill.PatternType = ExcelFillStyle.Solid;
+            ws.Cells["V1:X2"].Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml(greenHtmlColor));
+
             ws.Cells["Y2"].Value = "TagMapping";
 
-            ws.Cells["A1:H2"].Style.Fill.PatternType = ExcelFillStyle.Solid;
-            ws.Cells["A1:H2"].Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml(@"#90EE90"));
-            ws.Cells["A1:H1"].Merge = true;
+            ws.Cells["Y2"].Style.Fill.PatternType = ExcelFillStyle.Solid;
+            ws.Cells["Y2"].Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml(greenHtmlColor));
+
+            // Border
+            ws.Cells["A2:Y2"].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+            ws.Cells["A2:Y2"].Style.Border.Left.Style = ExcelBorderStyle.Thin;
+            ws.Cells["A2:Y2"].Style.Border.Right.Style = ExcelBorderStyle.Thin;
+            ws.Cells["A2:Y2"].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+
+            ws.Cells["A2:Y2"].AutoFilter = true;
         }
     }
 }
