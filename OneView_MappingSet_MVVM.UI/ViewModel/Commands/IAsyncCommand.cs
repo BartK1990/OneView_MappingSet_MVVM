bc7f5865b049a4449ff6e707f271b92ef09f2dd2
Bartosz Kuriata <kuriata.bartosz@gmail.com>
2021-08-22 19:09:47 +0200

    Added RaiseCanExecuteChanged to interface

--- OneView_MappingSet_MVVM.UI/ViewModel/Commands/IAsyncCommand.cs
+++ OneView_MappingSet_MVVM.UI/ViewModel/Commands/IAsyncCommand.cs
@@ -6,11 +6,13 @@ namespace OneView_MappingSet_MVVM.UI.ViewModel.Commands
     public interface IAsyncCommand : ICommand
     {
         Task ExecuteAsync();
+        void RaiseCanExecuteChanged();
     }
 
     public interface IAsyncCommand<T> : ICommand
     {
         Task ExecuteAsync(T parameter);
         bool CanExecute(T parameter);
+        void RaiseCanExecuteChanged();
     }
 }
